c54e7592bd540aa51d0179e75df02c5147f56411
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-18 20:03:09 +0300

    Update and rename symbols.json to data/models.js

---
+++ data/models.js
@@ -0,0 +1,8 @@
+// data/models.js
+
+const MODELS = {
+  A: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  B: 'ABCDEFGHIJKLMNOPQRSTUVWXYZâÇçĞğİıîÖöŞşÜüéàèûôœñáíú¿¡ºªãõÄäÜßëîìòøæØå',
+  C: ['-', ' ', ''], // тире, пробел, без символа
+  9: '0123456789'
+};
