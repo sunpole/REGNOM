a76dae420d581306a635f72d77e46414878e9c2c
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-18 20:02:49 +0300

    Update and rename germany.json to data/countries.js

---
+++ data/countries.js
@@ -0,0 +1,16 @@
+// data/countries.js
+
+const countryDatabase = [
+  {
+    name: "БЕЛАРУСЬ",
+    code: "BLR",
+    masks: ["AAc9999", "AAc9c9999"],
+    recommended: ["AA 999", "AA-9 9999"]
+  },
+  {
+    name: "РОССИЯ",
+    code: "RUS",
+    masks: ["AAc999c999", "AAc9999c999"],
+    recommended: ["AA-999 999", "AA 9999 999"]
+  }
+];
