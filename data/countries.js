963186f9027565c4d243d8feff5f7fe644cc3734
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2025-06-18 21:15:47 +0300

    Update countries.js

--- data/countries.js
+++ data/countries.js
@@ -2,15 +2,303 @@
 
 export const countryDatabase = [
   {
-    name: "БЕЛАРУСЬ",
-    code: "BLR",
-    masks: ["AAc9999", "AAc9c9999"],
-    recommended: ["AA 999", "AA-9 9999"]
+    name: "🇦🇱 Албания",
+    code: "ALB",
+    masks: ["A 9999"],
+    recommended: ["A 9999"] // Модель A
   },
   {
-    name: "РОССИЯ",
+    name: "🇦🇩 Андорра",
+    code: "AND",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇦🇲 Армения",
+    code: "ARM",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇦🇹 Австрия",
+    code: "AUT",
+    masks: ["AA 9999", "A 99999"],
+    recommended: ["AA 9999"]
+  },
+  {
+    name: "🇦🇿 Азербайджан",
+    code: "AZE",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇧🇾 Беларусь",
+    code: "BLR",
+    masks: ["AAc9999", "AAc9c9999"],
+    recommended: ["AA 999"]
+  },
+  {
+    name: "🇧🇪 Бельгия",
+    code: "BEL",
+    masks: ["A 9999", "A9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇧🇦 Босния и Герцеговина",
+    code: "BIH",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇧🇬 Болгария",
+    code: "BGR",
+    masks: ["A 9999", "A 9 999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇭🇷 Хорватия",
+    code: "HRV",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇨🇾 Кипр",
+    code: "CYP",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇨🇿 Чехия",
+    code: "CZE",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇩🇰 Дания",
+    code: "DNK",
+    masks: ["A 9999", "A-9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇪🇪 Эстония",
+    code: "EST",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇫🇮 Финляндия",
+    code: "FIN",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇫🇷 Франция",
+    code: "FRA",
+    masks: ["AA 9999", "A 99999"],
+    recommended: ["AA 9999"]
+  },
+  {
+    name: "🇬🇪 Грузия",
+    code: "GEO",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇩🇪 Германия",
+    code: "DEU",
+    masks: ["B 9999", "B 99", "B9999"],
+    recommended: ["B 9999"]
+  },
+  {
+    name: "🇬🇷 Греция",
+    code: "GRC",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇭🇺 Венгрия",
+    code: "HUN",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇮🇸 Исландия",
+    code: "ISL",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇮🇪 Ирландия",
+    code: "IRL",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇮🇹 Италия",
+    code: "ITA",
+    masks: ["A 99999", "AA 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇰🇿 Казахстан",
+    code: "KAZ",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇽🇰 Косово",
+    code: "XKX",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇱🇻 Латвия",
+    code: "LVA",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇱🇮 Лихтенштейн",
+    code: "LIE",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇱🇹 Литва",
+    code: "LTU",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇱🇺 Люксембург",
+    code: "LUX",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇲🇹 Мальта",
+    code: "MLT",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇲🇩 Молдова",
+    code: "MDA",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇲🇨 Монако",
+    code: "MCO",
+    masks: ["AA 9999", "A 9999"],
+    recommended: ["AA 9999"]
+  },
+  {
+    name: "🇲🇪 Черногория",
+    code: "MNE",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇳🇱 Нидерланды",
+    code: "NLD",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇲🇰 Северная Македония",
+    code: "MKD",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇳🇴 Норвегия",
+    code: "NOR",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇵🇱 Польша",
+    code: "POL",
+    masks: ["A 99999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇵🇹 Португалия",
+    code: "PRT",
+    masks: ["99 99 99", "AA 99"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇷🇴 Румыния",
+    code: "ROU",
+    masks: ["B 9999"],
+    recommended: ["B 9999"]
+  },
+  {
+    name: "🇷🇺 Россия",
     code: "RUS",
-    masks: ["AAc999c999", "AAc9999c999"],
-    recommended: ["AA-999 999", "AA 9999 999"]
+    masks: ["A 9999", "AA 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇸🇲 Сан-Марино",
+    code: "SMR",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇷🇸 Сербия",
+    code: "SRB",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇸🇰 Словакия",
+    code: "SVK",
+    masks: ["B 9999"],
+    recommended: ["B 9999"]
+  },
+  {
+    name: "🇸🇮 Словения",
+    code: "SVN",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇪🇸 Испания",
+    code: "ESP",
+    masks: ["9999 A", "9999 AA", "A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇸🇪 Швеция",
+    code: "SWE",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇨🇭 Швейцария",
+    code: "CHE",
+    masks: ["A 999 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇺🇦 Украина",
+    code: "UKR",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇬🇧 Великобритания",
+    code: "GBR",
+    masks: ["A 999 9999", "A99 999"],
+    recommended: ["A 9999"]
+  },
+  {
+    name: "🇻🇦 Ватикан",
+    code: "VAT",
+    masks: ["A 9999"],
+    recommended: ["A 9999"]
   }
 ];
